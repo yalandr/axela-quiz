cc71ed5809247c8be37f6a125da36c11f6480124
yalandr <52127093+yalandr@users.noreply.github.com>
2022-11-25 10:46:50 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,21 @@
 //     });
 // });
 
-let btnNext = document.querySelectorAll('.btn-next');
-let quizStep = document.querySelectorAll('.quiz-step');
+const btnNext = document.querySelectorAll('.btn-next');
+const quizStep = document.querySelectorAll('.quiz-step');
+const arrowBack = document.querySelector('.arrow-back-img');
+const progressBarLine = document.querySelector('.progress-bar-line');
+const progressPercentage = document.querySelector('.progress-percentage');
+let index = 0;
 
-// btnNext.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     console.log(quizStep);
-// })
-console.log(quizStep);
+btnNext.forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+        e.preventDefault();
+        quizStep.item(index).classList.remove('active');
+        quizStep.item(index+1).classList.add('active');
+        arrowBack.style.opacity = '1';
+        index++;
+        progressBarLine.style.width = 100 / quizStep.length + '%';
+        progressPercentage.innerText = Math.ceil(100 / quizStep.length) + '%';
+    })
+})
